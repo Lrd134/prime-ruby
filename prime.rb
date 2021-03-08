f8274534818f783905e3f6e9a94478bf84fb4e53
Lrd134 <larryc3200@gmail.com>
2021-03-07 21:32:00 -0500

    commit tree 197658360705df4876fa0a49227ee709cf19297b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+    n = 2
+    while n < num
+        if num % n == 0
+            return false
+        end
+        n+= 1
+        
+    end 
+    num <= 1 ? false : true
+end
